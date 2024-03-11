d6155c25c1ebed9cfd6fdc485cfd41256d9782d2
akkauntclever <fncps777@gmail.com>
2024-03-11 15:20:44 +0300

    + username

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,12 @@
 //Copyright 2024 Trifonov Ivan fncps777@gmail.com
 
 #include <iostream>
+#include <string>
 
 int main() {
-  std::cout << "Hello, world!!" << std::endl;
+  std::cout << "Enter your name:" << std::endl;
+  std::string username;
+  std::getline(std::cin, username);
+  std::cout << "Hello, world from " << username << std::endl;
   return 0;
 }
